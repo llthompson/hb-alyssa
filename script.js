b892900dc1c967a9cfe57b023dc3ed83d6401927
llthompson <lisalacythompson+github@gmail.com>
2023-08-23 19:58:06 -0500

    cute

---
+++ script.js
@@ -0,0 +1,16 @@
+//  // Wait for the DOM to be ready
+//  document.addEventListener('DOMContentLoaded', () => {
+//     // Initialize modals
+//     const modals = document.querySelectorAll('.modal');
+//     M.Modal.init(modals);
+
+//     // Initialize the modal trigger
+//     const modalTrigger = document.querySelector('.modal-trigger');
+//     M.Modal.getInstance(modalTrigger);
+
+//     // Add a click event listener to the modal trigger
+//     modalTrigger.addEventListener('click', () => {
+//       // Open the modal
+//       M.Modal.getInstance(modalTrigger).open();
+//     });
+//   });

3d450c4adee5627c0dd093ff79aa469d236102e3
llthompson <lisalacythompson+github@gmail.com>
2023-08-31 15:59:40 -0500

    count

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-//  // Wait for the DOM to be ready
-//  document.addEventListener('DOMContentLoaded', () => {
-//     // Initialize modals
-//     const modals = document.querySelectorAll('.modal');
-//     M.Modal.init(modals);
+  // Function to set or update the visitor count
+  function setVisitorCount(count) {
+    document.getElementById('visitorCount').textContent = count;
+}
 
-//     // Initialize the modal trigger
-//     const modalTrigger = document.querySelector('.modal-trigger');
-//     M.Modal.getInstance(modalTrigger);
+// Function to increment the visitor count
+function incrementVisitorCount() {
+    // Check if a 'visitorCount' cookie exists
+    let count = parseInt(getCookie('visitorCount')) || 0;
+    
+    // Increment the count
+    count++;
+    
+    // Update the 'visitorCount' cookie with the new count
+    document.cookie = 'visitorCount=' + count;
+    
+    // Update the display
+    setVisitorCount(count);
+}
 
-//     // Add a click event listener to the modal trigger
-//     modalTrigger.addEventListener('click', () => {
-//       // Open the modal
-//       M.Modal.getInstance(modalTrigger).open();
-//     });
-//   });
+// Function to get a cookie by name
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+            return cookie.substring(name.length + 1);
+        }
+    }
+    return null;
+}
+
+// Initialize the counter on page load
+window.onload = function () {
+    let count = parseInt(getCookie('visitorCount')) || 0;
+    setVisitorCount(count);
+};
+
+// Increment the counter on each page visit
+incrementVisitorCount();
